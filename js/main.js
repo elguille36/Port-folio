173aedd8cccfdc017dff883bc54c2cbdfccf9709
guillermo perez <136027196+elguille36@users.noreply.github.com>
2023-08-27 20:11:27 -0300

    JS funciones de mi portafolio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+const activacion=document.querySelector('nav-menu');
+const menuOpcion=document.querySelector('.btn');
+
+
+menuOpcion.addEventListener('click',function(){
+    
+document.body.classList.toggle('menu-active');
+
+})
