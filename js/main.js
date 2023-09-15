23c95bf92e376d8c93ee8096fcf3c599b879e974
guillermo perez <136027196+elguille36@users.noreply.github.com>
2023-09-14 21:22:41 -0300

    se agrego nueva funcion

--- js/main.js
+++ js/main.js
@@ -1,49 +1,74 @@
 /* funcion al boton de navegadores en modo Mobile*/
-const activacion=document.querySelector('nav-menu');
-const menuOpcion=document.querySelector('.btn');
+const activacion = document.querySelector('nav-menu');
+const menuOpcion = document.querySelector('.btn');
 
 
-menuOpcion.addEventListener('click',function(){
-    
-document.body.classList.toggle('menu-active');
+menuOpcion.addEventListener('click', function () {
 
-})
+    document.body.classList.toggle('menu-active');
 
-/* funcion de mensaje de envio de formulario*/ 
+
+});
+
+/* funcion de mensaje de envio de formulario*/
 
 const $form = document.querySelector('.formulario');
 
-$form.addEventListener('submit',enviarSubmit)
+$form.addEventListener('submit', enviarSubmit)
 
-async function enviarSubmit(e){
+async function enviarSubmit(e) {
 
     e.preventDefault()
 
-      const form = new FormData(this)
-     const response = await fetch(this.action,{
-      method:this.method,
-      body:form,
-      headers:{
-          'Accept': 'application/json'
-      }
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
     })
 
-    if(response.ok){
+    if (response.ok) {
         this.reset()
-alert('Gracias por Contactarme, Pronto le Daré Un Retorno :)')
+        alert('Gracias por Contactarme, Pronto le Daré Un Retorno :)')
     }
 
 }
 
 /* Funcion de modo Oscuro*/
-const modoOscuro=document.querySelector('.dark');
+const modoOscuro = document.querySelector('.dark');
 
-const body= document.body;
+const body = document.body;
 
-modoOscuro.addEventListener('click',() => {
+modoOscuro.addEventListener('click', () => {
 
     body.classList.toggle('dark-mode');
 
 
 });
+
+
+const colorMenu = document.querySelectorAll('.enlace-menu');
+
+colorMenu.forEach((colorMen) => {
+    
+    colorMen.style.color ='black';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
